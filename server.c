a3de787bf43cf3c3bbf8826e8329314eac5b65f5
Lucas Yang <lyang420@umd.edu>
2024-07-09 15:34:47 -0400

    added documentation

--- server.c
+++ server.c
@@ -1,3 +1,7 @@
+/* `server.c` is a basic TCP server written in C supporting both IPv4 and IPv6
+requests. After receiving data from a client, the server sends it back with a
+greeting message acknowledging receipt. */
+
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,6 +18,9 @@ int main() {
 	char buf[buf_size];
 	const char *greeting = "Hey, you sent me: ";
 
+	/* Configure host address, which is the localhost for now. Use `memset_s` and
+	not `memset`. */
+
 	memset_s(&hints, sizeof(hints), 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET6;
 	hints.ai_socktype = SOCK_STREAM;
@@ -25,11 +32,17 @@ int main() {
 		exit(1);
 	}
 
+	/* Create socket. */
+
 	sockfd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
 	if (sockfd == -1) {
 		perror("Failed to create server socket");
 		exit(1);
 	}
+
+	/* Activate dual stack configuration (notice setting of family to `AF_INET6`
+	and not `AF_INET` at line 25 above and usage of `AI_PASSIVE` flag at line
+	27). I am told this may not be supported on all operating systems. */
 
 	option = 0;
 	r = setsockopt(sockfd, IPPROTO_IPV6, IPV6_V6ONLY, (void *) &option, sizeof(option));
@@ -37,6 +50,9 @@ int main() {
 		perror("Failed to activate dual stack configuration");
 		exit(1);
 	}
+
+	/* Once configured, bind server socket to host address and listen for
+	incoming requests on the network. */
 
 	r = bind(sockfd, server->ai_addr, server->ai_addrlen);
 	if (r == -1) {
@@ -49,6 +65,8 @@ int main() {
 		perror("Failed listening for incoming requests");
 		exit(1);
 	}
+
+	/* After accepting request, process into buffer and send it back. */
 
 	client_len = sizeof(client_address);
 	clientfd = accept(sockfd, &client_address, &client_len);
@@ -63,6 +81,8 @@ int main() {
 		r = send(clientfd, greeting, strlen(greeting), 0);
 		r = send(clientfd, buf, strlen(buf), 0);
 	}
+
+	/* Close file descriptors and open sockets before terminating program. */
 
 	freeaddrinfo(server);
 	close(clientfd);
