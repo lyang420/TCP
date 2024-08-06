166a92b1443834f1591aac73e20550afca57ea83
Lucas Yang <lyang420@umd.edu>
2024-08-05 20:41:03 -0400

    attempt connect client with server on host address

--- client.c
+++ client.c
@@ -13,24 +13,36 @@ message to `server.c`. */
 #define FALSE 0
 
 int main(int argc, char *argv[]) {
-	struct addrinfo hints, *host;
-	int r, sockfd, i;
-	const int buf_size = 1024;
-	char buf[buf_size];
+	/* host address configuration */
+	const char *port = "8080";
+	char *host;
+	struct addrinfo hints, *server;
+	/* client configuration */
+	int r, sock_fd, done;
+	char buffer[BUFSIZ];
+	fd_set read_fd;
 
+	if (argc < 2) {
+		fprintf(stderr, "Usage: client hostname\n");
+		exit(1);
+	}
+	host = argv[1];
+
+	printf("Searching for running servers on %s... ", host);
 	memset_s(&hints, sizeof(hints), 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-	r = getaddrinfo(0, "8080", &hints, &host);
+	r = getaddrinfo(host, port, &hints, &server);
 	if (r != 0) {
 		perror("client getaddrinfo() failed");
 		exit(1);
 	}
+	puts("found");
 
 	/* Create socket. */
 
-	sockfd = socket(host->ai_family, host->ai_socktype, host->ai_protocol);
-	if (sockfd == -1) {
+	sock_fd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+	if (sock_fd == -1) {
 		perror("client socket() failed");
 		exit(1);
 	}
@@ -38,41 +50,46 @@ int main(int argc, char *argv[]) {
 	/* Once socket is configured and connected to host address, client can start
 	interacting with server. */
 
-	r = connect(sockfd, host->ai_addr, host->ai_addrlen);
+	r = connect(sock_fd, server->ai_addr, server->ai_addrlen);
+	freeaddrinfo(server);
 	if (r == -1) {
 		perror("client connect() failed");
 		exit(1);
 	}
 
-	for (;;) {
-		printf(" > ");
-		fgets(buf, buf_size, stdin);
-		if (buf[0] == '\n') {
-			break;
-		}
-		for (i = 0; i < buf_size; i++) {
-			if (buf[i] == '\n') {
-				buf[i] = '\0';
-				break;
-			}
-		}
-		if (strcmp(buf, "exit") == 0 || strcmp(buf, "quit") == 0) {
-			break;
-		}
-		r = send(sockfd, buf, strlen(buf), 0);
+	done = FALSE;
+	while (!done) {
+		FD_ZERO(&read_fd);
+		FD_SET(sock_fd, &read_fd);
+		FD_SET(0, &read_fd);
+		r = select(sock_fd + 1, &read_fd, NULL, NULL, 0);
 		if (r == -1) {
-			perror("client send() failed");
+			perror("client select() failed");
 			exit(1);
 		}
-		r = recv(sockfd, buf, buf_size, 0);
-		buf[r] = '\0';
-		printf("%s\n", buf);
+		if (FD_ISSET(sock_fd, &read_fd)) {
+			r = recv(sock_fd, buffer, BUFSIZ, 0);
+			if (r < 1) {
+				puts("client recv() closed");
+				break;
+			}
+			buffer[r] = '\0';
+			printf("%s", buffer);
+		}
+		if (FD_ISSET(0, &read_fd)) {
+			if (fgets(buffer, BUFSIZ, stdin) == NULL) {
+				putchar('\n');
+			} else if (strcmp(buffer, "disconnect\n") == 0) {
+				done = TRUE;
+			} else {
+				send(sock_fd, buffer, strlen(buffer), 0);
+			}
+		}
 	}
 
 	/* Close file descriptors and open sockets before terminating program. */
 
-	freeaddrinfo(host);
-	close(sockfd);
+	close(sock_fd);
 
 	return 0;
 }
